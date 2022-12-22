b661477e8be0b35a073ce6c7da9c45c924be189a
xxRohitxx <rohitrs319791@gmail.com>
2022-12-23 04:11:58 +0530

    adding frontend

---
+++ coding/Components/askdoubt.jsx
@@ -0,0 +1,68 @@
+
+import { Button, Input ,Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,useDisclosure, Box, Modal,
+  } from '@chakra-ui/react';
+  import React, { use, useState } from 'react';
+  import Chat from './chat';
+
+  const Askdoubt = () => {
+    const [username,setusername]=useState("")
+    const [user,setuser]=useState("")
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isActive,setActive]=useState(false)
+   
+    const btnRef = React.useRef()
+
+
+
+    const userEntertheForum=()=>{
+        if(!username)return
+        setuser(username)
+        onClose()
+        setActive(true)
+    }
+
+
+    if(user)return <Chat user={user}/>
+
+    return (
+        <Box   >
+           
+
+
+            <Button  size="lg" ref={btnRef} colorScheme='orange' onClick={onOpen}>
+        Ask for Doubt
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="lg"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton  />
+          <DrawerHeader fontWeight="bold" bgColor="messenger.100" fontSize="2xl" >
+            Tell me Your name
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Input mt="30" h="60" fontSize="4xl" onChange={(e)=>{setusername(e.target.value)}}/>
+            <Button mt="5"onClick={userEntertheForum} colorScheme="facebook">Enter to the Forum</Button>
+          </DrawerBody>
+
+          
+        </DrawerContent>
+      </Drawer>
+
+
+        </Box>
+          )
+}
+
+export default Askdoubt
